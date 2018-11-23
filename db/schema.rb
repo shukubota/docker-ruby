6a7eae60c38b0abdae883d43e0de908985f430f3
shukubota <kubotashu0421@gmail.com>
2018-11-23 20:46:27 +0900

    migrate修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181111144332) do
+ActiveRecord::Schema.define(version: 20181112123114) do
 
   create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20181111144332) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name",       null: false
+    t.index ["id"], name: "index_users_on_id", unique: true, using: :btree
   end
 
 end
